2b988357451a79cc520a0035816a9050d622d887
rakheyl <89585341+rakheyl@users.noreply.github.com>
2022-06-07 21:27:46 +0200

    Use TypeScript access scope

--- src/index.ts
+++ src/index.ts
@@ -11,107 +11,92 @@ export interface WorkerOptions {
 }
 
 export class Worker {
-	#epoch: bigint;
-	#workerId: bigint;
-	#workerIdBits: bigint;
-	#maxWorkerId: bigint;
-	#datacenterId: bigint;
-	#datacenterIdBits: bigint;
-	#maxDatacenterId: bigint;
-	#sequence: bigint;
-	#sequenceBits: bigint;
-	#workerIdShift: bigint;
-	#datacenterIdShift: bigint;
-	#timestampLeftShift: bigint;
-	#sequenceMask: bigint;
-	#lastTimestamp: bigint = -1n;
+	public readonly epoch: bigint;
+	public readonly workerId: bigint;
+	public readonly workerIdBits: bigint;
+	public readonly maxWorkerId: bigint;
+	public readonly datacenterId: bigint;
+	public readonly datacenterIdBits: bigint;
+	public readonly maxDatacenterId: bigint;
+	protected sequence: bigint;
+	public readonly sequenceBits: bigint;
+	public readonly workerIdShift: bigint;
+	public readonly datacenterIdShift: bigint;
+	public readonly timestampLeftShift: bigint;
+	public readonly sequenceMask: bigint;
+	protected lastTimestamp: bigint = -1n;
 
-	constructor(workerId: NumberOrBigInt = 0n, datacenterId: NumberOrBigInt = 0n, options?: WorkerOptions) {
+	public constructor(workerId: NumberOrBigInt = 0n, datacenterId: NumberOrBigInt = 0n, options?: WorkerOptions) {
 		// Epoch
-		this.#epoch = BigInt(options?.epoch ?? 1609459200000);
+		this.epoch = BigInt(options?.epoch ?? 1609459200000);
 
 		// Worker
-		this.#workerId = BigInt(workerId);
-		this.#workerIdBits = BigInt(options?.workerIdBits ?? 5);
-		this.#maxWorkerId = -1n ^ (-1n << this.#workerIdBits);
-		if (this.#workerId < 0 || this.#workerId > this.#maxWorkerId) {
+		this.workerId = BigInt(workerId);
+		this.workerIdBits = BigInt(options?.workerIdBits ?? 5);
+		this.maxWorkerId = -1n ^ (-1n << this.workerIdBits);
+
+		if (this.workerId < 0 || this.workerId > this.maxWorkerId) {
 			throw new Error(
-				'With ' +
-					this.#workerIdBits.toString() +
-					" bits, worker id can't be greater than " +
-					this.#maxWorkerId.toString() +
-					' or less than 0',
+				'With ' + this.workerIdBits.toString() + " bits, worker id can't be greater than " + this.maxWorkerId.toString() + ' or less than 0',
 			);
 		}
 
 		// Datacenter
-		this.#datacenterId = BigInt(datacenterId);
-		this.#datacenterIdBits = BigInt(options?.datacenterIdBits ?? 5);
-		this.#maxDatacenterId = -1n ^ (-1n << this.#datacenterIdBits);
-		if (this.#datacenterId > this.#maxDatacenterId || this.#datacenterId < 0) {
+		this.datacenterId = BigInt(datacenterId);
+		this.datacenterIdBits = BigInt(options?.datacenterIdBits ?? 5);
+		this.maxDatacenterId = -1n ^ (-1n << this.datacenterIdBits);
+		if (this.datacenterId > this.maxDatacenterId || this.datacenterId < 0) {
 			throw new Error(
 				'With ' +
-					this.#datacenterIdBits.toString() +
+					this.datacenterIdBits.toString() +
 					" bits, datacenter id can't be greater than " +
-					this.#maxDatacenterId.toString() +
+					this.maxDatacenterId.toString() +
 					' or less than 0',
 			);
 		}
 
 		// Sequence
-		this.#sequence = BigInt(options?.sequence ?? 0);
-		this.#sequenceBits = BigInt(options?.sequenceBits ?? 12);
-		this.#sequenceMask = -1n ^ (-1n << this.#sequenceBits);
+		this.sequence = BigInt(options?.sequence ?? 0);
+		this.sequenceBits = BigInt(options?.sequenceBits ?? 12);
+		this.sequenceMask = -1n ^ (-1n << this.sequenceBits);
 
 		// Shift
-		this.#workerIdShift = this.#sequenceBits;
-		this.#datacenterIdShift = this.#sequenceBits + this.#workerIdBits;
-		this.#timestampLeftShift = this.#sequenceBits + this.#workerIdBits + this.#datacenterIdBits;
+		this.workerIdShift = this.sequenceBits;
+		this.datacenterIdShift = this.sequenceBits + this.workerIdBits;
+		this.timestampLeftShift = this.sequenceBits + this.workerIdBits + this.datacenterIdBits;
 	}
 
-	get workerId(): bigint {
-		return this.#workerId;
+	public get currentSequence(): bigint {
+		return this.sequence;
 	}
 
-	get datacenterId(): bigint {
-		return this.#datacenterId;
-	}
-
-	get currentSequence(): bigint {
-		return this.#sequence;
-	}
-
-	get lastTimestamp(): bigint {
-		return this.#lastTimestamp;
-	}
-
-	nextId(): bigint {
+	public nextId(): bigint {
 		let timestamp = Worker.now();
 
-		if (timestamp < this.#lastTimestamp) {
-			throw new Error("Clock moved backwards. Can't generate new ID for " + (this.#lastTimestamp - timestamp).toString() + 'milliseconds.');
+		if (timestamp < this.lastTimestamp) {
+			throw new Error("Clock moved backwards. Can't generate new ID for " + (this.lastTimestamp - timestamp).toString() + 'milliseconds.');
 		}
 
-		if (timestamp === this.#lastTimestamp) {
-			this.#sequence = (this.#sequence + 1n) & this.#sequenceMask;
-			if (this.#sequence === 0n) {
-				timestamp = this.tilNextMillis(this.#lastTimestamp);
+		if (timestamp === this.lastTimestamp) {
+			this.sequence = (this.sequence + 1n) & this.sequenceMask;
+			if (this.sequence === 0n) {
+				timestamp = Worker.untilNextMillis(this.lastTimestamp);
 			}
 		} else {
-			this.#sequence = 0n;
+			this.sequence = 0n;
 		}
 
-		this.#lastTimestamp = timestamp;
+		this.lastTimestamp = timestamp;
 
 		return (
-			((timestamp - this.#epoch) << this.#timestampLeftShift) |
-			(this.#datacenterId << this.#datacenterIdShift) |
-			(this.#workerId << this.#workerIdShift) |
-			this.#sequence
+			((timestamp - this.epoch) << this.timestampLeftShift) |
+			(this.datacenterId << this.datacenterIdShift) |
+			(this.workerId << this.workerIdShift) |
+			this.sequence
 		);
 	}
 
-	tilNextMillis(lastTimestamp: bigint): bigint {
+	public static untilNextMillis(lastTimestamp: bigint): bigint {
 		let timestamp;
 
 		do {
@@ -121,7 +106,7 @@ export class Worker {
 		return timestamp;
 	}
 
-	static now(): bigint {
+	public static now(): bigint {
 		return BigInt(Date.now());
 	}
 }
